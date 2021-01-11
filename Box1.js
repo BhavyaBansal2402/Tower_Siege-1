9e67c2884d25b61035437161cf9b07909b13bbae
BhavyaBansal2402 <72801817+BhavyaBansal2402@users.noreply.github.com>
2021-01-11 19:44:48 +0530

    Add files via upload

---
+++ Box1.js
@@ -0,0 +1,14 @@
+class Box1 extends BaseClass {
+    constructor(x, y, width, height)
+    {
+      super(x,y,width,height);
+      //this.image = loadImage("sprites/wood1.png");
+    }
+
+    display()
+    {
+        fill("pink");
+        super.display();
+    }
+  }
+  
---
+++ Box2.js
@@ -0,0 +1,14 @@
+class Box2 extends BaseClass {
+    constructor(x, y, width, height)
+    {
+      super(x,y,width,height);
+      //this.image = loadImage("sprites/wood1.png");
+    }
+
+    display()
+    {
+        fill("green");
+        super.display();
+    }
+  }
+  
